6c122d0963a0baed534b84b134780c69fe77114f
Werner Hoch <werner.ho@gmx.de>
2008-08-13 12:59:15 +0200

    script to generate a hdf5 dataset for testing

---
+++ test/make_testhdf5.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*-Python-*-
+
+# dataplot - plot hierachical datasets
+# Copyright (C) 2008 Werner Hoch <werner.ho@gmx.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import numpy
+import tables
+
+
+
+#################### MAIN
+
+f = tables.openFile("data/testdata.hdf5","w")
+
+f.createGroup('/', 'sin_cos')
+
+# create some arrays with different sizes
+# this is used to do some performance tests
+for n in [100,1000,10000,100000,1000000]:
+    x = numpy.linspace(-10,10,n)
+    y_sin1 = numpy.sin(x)
+    y_sin2 = 0.9*numpy.sin(2*x)
+    y_cos1 = 1.1*numpy.sin(3.5*x)
+    y_cos2 = 1*numpy.sin(5*x)
+    a = numpy.vstack([x, y_sin1, y_sin2, y_cos1, y_cos2])
+
+    f.createArray('/sin_cos',"N%i"%n, a)
+
+# create some complex datasets
+# this will be used to test operators for complex data like abs, real, imag, ..
+f.createGroup('/', 'complex')
+x = numpy.linspace(-10,10,1000)
+y_sin1 = numpy.sin(x)
+y_sin2 = 0.9*numpy.sin(2*x)
+y_cos1 = 1.1*numpy.sin(3.5*x)
+y_cos2 = 1*numpy.sin(5*x)
+a = numpy.vstack([x, 1j*y_sin1 + y_cos1, 1j*y_sin2 + y_cos2])
+f.createArray('/complex', 'sin', a)
+
+f.close()
